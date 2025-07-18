3c11856cb15c61513662457da3abbf6e34cb25ef
Jordan <Jordan_lei@hotmail.com>
2025-07-18 16:15:19 -0700

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/web/', // Important for GitHub Pages
+  base: '/src/main.jsx/', // Important for GitHub Pages
   plugins: [react()],
 });
