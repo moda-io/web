af0539a1d0c5c90ac85bf08a17e401722191f6c6
Jordan <Jordan_lei@hotmail.com>
2025-07-24 19:14:14 -0700

    config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/moda-ia/web/',
+    base: '/web/',
   plugins: [react()],
 }); 
